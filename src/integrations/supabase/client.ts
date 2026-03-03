515ea9dde4b85b2d46d6691d154c24c18f7c29bd
Itsjayrajgorrimpl <jayrajgorr@gmail.com>
2026-03-04 02:13:01 +0530

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -4,7 +4,8 @@ import type { Database } from './types';
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
 const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient<Database>(
+export const supabase = null as any;
+##export const supabase = createClient<Database>(
   SUPABASE_URL,
   SUPABASE_ANON_KEY,
   {
