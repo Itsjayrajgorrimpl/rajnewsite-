fb68e7fe632fabdd7b646bc495324e8c068f44fd
Itsjayrajgorrimpl <jayrajgorr@gmail.com>
2026-03-03 23:07:27 +0530

    Create Layout.tsx

---
+++ src/components/Layout.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import FooterFAQ from "./FooterFAQ";
+import WhatsAppButton from "./WhatsAppButton";
+
+const Layout = ({ children }: { children: ReactNode }) => (
+  <div className="flex min-h-screen flex-col">
+    <Header />
+    <main className="flex-1">{children}</main>
+    <FooterFAQ />
+    <Footer />
+    <WhatsAppButton />
+  </div>
+);
+
+export default Layout;
